de65be1ac29eb61e751fc7abd707ebf9d906c999
Cédric Messiant <cedricmessiant@ecreall.com>
2015-08-28 14:21:39 +0200

    Add pragma no cover in post_install

--- src/collective/querynextprev/setuphandlers.py
+++ src/collective/querynextprev/setuphandlers.py
@@ -4,5 +4,5 @@
 def post_install(context):
     """Post install script"""
     if context.readDataFile('collectivequerynextprev_default.txt') is None:
-        return
+        return  # pragma: no cover
     # Do something during the installation of this package
